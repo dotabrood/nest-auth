2cc2f1a1084f98aff6b8525e2ce3f38d1ef86bf0
rtn1.13 <daewoo1488@gmail.com>
2025-10-05 01:20:39 +0300

    add oauth auth (google/yandex)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { AuthModule } from './auth/auth.module'
+import { ProviderModule } from './auth/provider/provider.module'
 import { IS_DEV_ENV } from './libs/common/utils/is-dev.util'
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { ProviderModule } from './provider/provider.module';
+import { PrismaModule } from './prisma/prisma.module'
+import { UserModule } from './user/user.module'
 
 @Module({
 	imports: [
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,16 +10,20 @@ import { AuthMethod, User } from '@prisma/__generated__'
 import { verify } from 'argon2'
 import { Request, Response } from 'express'
 
+import { PrismaService } from '@/prisma/prisma.service'
 import { UserService } from '@/user/user.service'
 
 import { LoginDto } from './dto/auth.dto'
 import { RegisterDto } from './dto/register.dto'
+import { ProviderService } from './provider/provider.service'
 
 @Injectable()
 export class AuthService {
 	public constructor(
 		private readonly userService: UserService,
-		private readonly configService: ConfigService
+		private readonly configService: ConfigService,
+		private readonly providerService: ProviderService,
+		private readonly prismaService: PrismaService
 	) {}
 
 	public async register(req: Request, dto: RegisterDto) {
@@ -58,6 +62,54 @@ export class AuthService {
 			throw new UnauthorizedException(
 				'Неверный пароль. Пожалуйста, попробуйте еще раз или восстановите пароль, если забыли его.'
 			)
+		}
+
+		return this.saveSession(req, user)
+	}
+
+	public async extractProfileFromCode(
+		req: Request,
+		provider: string,
+		code: string
+	) {
+		const providerInstance = this.providerService.findByService(provider)
+		const profile = await providerInstance.findUserByCode(code)
+
+		const account = await this.prismaService.account.findFirst({
+			where: {
+				id: profile.id,
+				provider: profile.provider
+			}
+		})
+
+		let user = account?.userId
+			? await this.userService.findById(account.userId)
+			: null
+
+		if (user) {
+			return this.saveSession(req, user)
+		}
+
+		user = await this.userService.create(
+			profile.email,
+			'',
+			profile.name,
+			profile.picture,
+			AuthMethod[profile.provider.toUpperCase()],
+			true
+		)
+
+		if (!account) {
+			await this.prismaService.account.create({
+				data: {
+					userId: user.id,
+					type: 'oauth',
+					provider: profile.provider,
+					accessToken: profile.access_token,
+					refreshToken: profile.refresh_token,
+					expiresAt: profile.expires_at
+				}
+			})
 		}
 
 		return this.saveSession(req, user)
